9be64762716403a043f59ba759c49c61dde5c9ee
Bernhard Diemer <bd@nedgex.com>
2021-03-24 17:11:12 +0100

    Initial Commit

---
+++ Challenge_16/Challenge_16App.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge_16App.swift
+//  Challenge_16
+//
+//  Created by Bernhard Diemer  on 24.03.21.
+//
+
+import SwiftUI
+
+@main
+struct Challenge_16App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
